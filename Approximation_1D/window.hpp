88611604f879a3a320ec23e136870fa4bc23fe72
mephistopheles-ilya <ilyuxa.popoff@yandex.ru>
2025-03-10 03:26:56 +0300

    good morning

--- Approximation_1D/window.hpp
+++ Approximation_1D/window.hpp
@@ -41,6 +41,10 @@ class MainWindow: public QMainWindow {
         double a = 0, b = 0;
         int n = 0, k = 0;
         int s = 0;
+        double F_max = 0, F_min = 0;
+        double* newton_coeff = nullptr;
+        double* d = nullptr;
+        double* c = nullptr;
 
     public:
 
@@ -87,6 +91,74 @@ class MainWindow: public QMainWindow {
 
     protected:
 
+        void memmory_management() {}
+
+        void Newton_approximation() {
+            delete[] newton_coeff;
+            newton_coeff = new double[n];
+            double x = a;
+            double delta_x = (b - a)/n;
+            for(int i = 0; i < n; ++i) {
+                newton_coeff[i] = f(x + i * delta_x);
+            }
+
+            for(int j = 1; j < n; ++j) {
+                for(int i = n - 1; i >= j; --i) {
+                    double delta_f = (newton_coeff[i] - newton_coeff[i - 1]) / (delta_x * j);
+                    newton_coeff[i] = delta_f;
+                }
+            }
+        }
+
+        double Newton_calck(double x) {
+            double res = newton_coeff[n - 1];
+            double delta_x = (b - a) / n;
+            for(int i = n - 2; i >= 0; --i) {
+                res *= (x - (a + i * delta_x));
+                res += newton_coeff[i];
+            }
+            return res;
+        }
+
+        int sign(double x) {
+            if (x > 0) return 1;
+            if (x < 0) return -1;
+            return 0;
+        }
+
+        void cube_approximation() {
+            delete[] d;
+            delete[] c;
+            d = new double[n - 2];
+            c = new double[4 * n];
+            double delta_x = (b - a) / n;
+            for(int i = 1; i < n - 1; ++i) {
+                double df1 = (f(a + i * delta_x) - f(a + (i - 1) * delta_x))/delta_x;
+                double df2 = (f(a + (i + 1) * delta_x) - f(a + i * delta_x))/delta_x;
+                if (df1 * df2 > 0) {
+                    d[i] = sign(df1) * ((df1 > df2) ? df2 : df1);
+                } else {
+                    d[i] = 0;
+                }
+            }
+            for(int i = 2; i < n - 2; ++i) {
+                c[4 * i + 0] = f(a  + i * delta_x);
+                c[4 * i + 1] = d[i];
+                double df = (f(a + (i + 1) * delta_x) - f(a + i * delta_x))/delta_x;
+                c[4 * i + 2] = (3 * df - 2 * d[i] - d[i + 1])/delta_x;
+                c[4 * i + 3] = (d[i] + d[i + 1] - 2 * df)/(delta_x * delta_x);
+            }
+        }
+        double cube_calc(double x) {
+            double delta_x = (b - a) / n;
+            int i = (a - x)/delta_x;
+            double x_i = a + i * delta_x;
+            return c[4 * i + 0] + c[4 * i + 1] * (x - x_i) + c[4 * i + 2] * (x - x_i) * (x - x_i) + c[4 * i + 3] * (x - x_i) * (x - x_i) * (x - x_i);
+        }
+
+
+
+
         void keyPressEvent(QKeyEvent *event) override {
             if (event->key() == Qt::Key_0) {
                 next_function();
@@ -128,8 +200,13 @@ class MainWindow: public QMainWindow {
 
         void paintEvent(QPaintEvent* ) override {
             QPainter painter (this);
-            QPen pen_blue(Qt::blue, 0, Qt::SolidLine); 
-            QPen pen_black(Qt::black, 0, Qt::SolidLine); 
+            QPen pen_blue(Qt::blue, 4, Qt::SolidLine); 
+            QPen pen_green(Qt::green, 4, Qt::SolidLine); 
+            QPen pen_black(Qt::black, 4, Qt::SolidLine);
+
+            memmory_management();
+            Newton_approximation();
+            cube_approximation();
 
             double x1 = 0, x2 = 0, y1 = 0, y2 = 0;
             double delta_y = 0, delta_x = (b - a)/width();
@@ -140,6 +217,15 @@ class MainWindow: public QMainWindow {
                     min_y = y1;
                 if (y1 > max_y)
                     max_y = y1;
+
+            }
+            for (x1 = a; x1 - b < 1.e-6; x1 += delta_x) {
+                y1 = Newton_calck(x1);
+                if (y1 < min_y)
+                    min_y = y1;
+                if (y1 > max_y)
+                    max_y = y1;
+
             }
 
             delta_y = 0.01 * (max_y - min_y);
@@ -152,9 +238,9 @@ class MainWindow: public QMainWindow {
 
             painter.setPen(pen_blue);
             x1 = a;
-            y1 = f (x1);
+            y1 = f(x1);
             for (x2 = x1 + delta_x; x2 - b < 1.e-6; x2 += delta_x) {
-                y2 = f (x2);
+                y2 = f(x2);
                 painter.drawLine(L2G(x1, y1), L2G(x2, y2));
                 x1 = x2;
                 y1 = y2;
@@ -162,6 +248,23 @@ class MainWindow: public QMainWindow {
             x2 = b;
             y2 = f(x2);
             painter.drawLine(L2G(x1, y1), L2G(x2, y2));
+
+            painter.setPen(pen_green);
+            x1 = a;
+            y1 = Newton_calck(x1);
+            for (x2 = x1 + delta_x; x2 - b < 1.e-6; x2 += delta_x) {
+                y2 = Newton_calck(x2);
+                painter.drawLine(L2G(x1, y1), L2G(x2, y2));
+                x1 = x2;
+                y1 = y2;
+            }
+            x2 = b;
+            y2 = Newton_calck(x2);
+            painter.drawLine(L2G(x1, y1), L2G(x2, y2));
+
+            F_min = min_y;
+            F_max = max_y;
+            updateMenuTitle();
 
         }
 
@@ -179,25 +282,32 @@ class MainWindow: public QMainWindow {
             QString title;
             switch (k) {
                 case 0: 
-                    title = QString("k = 0 f(x) = 1, n = %1, s = %2").arg(n).arg(s); 
+                    title = QString("k = 0 f(x) = 1, n = %1, s = %2, [a, b] = [%3, %4], [F_min, F_max] = [%5, %6]")
+                        .arg(n).arg(s).arg(a).arg(b).arg(F_min).arg(F_max); 
                     break;
                 case 1: 
-                    title = QString("k = 1 f(x) = x, n = %1, s = %2").arg(n).arg(s); 
+                    title = QString("k = 1 f(x) = x, n = %1, s = %2, [a, b] = [%3, %4], [F_min, F_max] = [%5, %6]")
+                        .arg(n).arg(s).arg(a).arg(b).arg(F_min).arg(F_max); 
                     break;
                 case 2: 
-                    title = QString("k = 2 f(x) = x^2, n = %1, s = %2").arg(n).arg(s); 
+                    title = QString("k = 2 f(x) = x^2, n = %1, s = %2, [a, b] = [%3, %4], [F_min, F_max] = [%5, %6]")
+                        .arg(n).arg(s).arg(a).arg(b).arg(F_min).arg(F_max); 
                     break;
                 case 3: 
-                    title = QString("k = 3 f(x) = x^3, n = %1, s = %2").arg(n).arg(s); 
+                    title = QString("k = 3 f(x) = x^3, n = %1, s = %2, [a, b] = [%3, %4], [F_min, F_max] = [%5, %6]")
+                        .arg(n).arg(s).arg(a).arg(b).arg(F_min).arg(F_max); 
                     break;
                 case 4: 
-                    title = QString("k = 4 f(x) = x^4, n = %1, s = %2").arg(n).arg(s); 
+                    title = QString("k = 4 f(x) = x^4, n = %1, s = %2, [a, b] = [%3, %4], [F_min, F_max] = [%5, %6]")
+                        .arg(n).arg(s).arg(a).arg(b).arg(F_min).arg(F_max); 
                     break;
                 case 5: 
-                    title = QString("k = 5 f(x) = exp(x), n = %1, s = %2").arg(n).arg(s); 
+                    title = QString("k = 5 f(x) = exp(x), n = %1, s = %2, [a, b] = [%3, %4], [F_min, F_max] = [%5, %6]")
+                        .arg(n).arg(s).arg(a).arg(b).arg(F_min).arg(F_max); 
                     break;
                 case 6: 
-                    title = QString("k = 6 f(x) = 1/(25*x^2 + 1), n = %1, s = %2").arg(n).arg(s); 
+                    title = QString("k = 6 f(x) = 1/(25*x^2 + 1), n = %1, s = %2, [a, b] = [%3, %4], [F_min, F_max] = [%5, %6]")
+                        .arg(n).arg(s).arg(a).arg(b).arg(F_min).arg(F_max); 
                     break;
             }
             functionsMenu->setTitle(title);
@@ -229,6 +339,8 @@ class MainWindow: public QMainWindow {
 
     public:
 
-        ~MainWindow() {};
+        ~MainWindow() {
+            delete[] newton_coeff;
+        };
 };
 
